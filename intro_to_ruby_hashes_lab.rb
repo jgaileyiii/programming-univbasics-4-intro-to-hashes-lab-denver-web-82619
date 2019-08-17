dd82046b595a350c1c9aff9441e76eff8d9d458d
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-17 18:22:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  read_from_hash = hash[key]
+  value = []
+  read_from_hash = (hash, :key => value)
   puts read_from_hash
   end
 
