18b88367d1ecb7240fa6f67f50827a07c51f56d6
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-17 18:46:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,9 +19,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  value = []
-  puts hash[value]
-end
+  if hash[key]
+    puts value
+  end
  
  
 def update_counting_hash(hash, key)
